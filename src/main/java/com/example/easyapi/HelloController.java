30a06453bd26e1e019b0dd1dc554a883981f1745
Tamara Murazyan <murazyan.tamara5566.com>
2025-03-06 01:05:36 +0400

    Change greeting

--- src/main/java/com/example/easyapi/HelloController.java
+++ src/main/java/com/example/easyapi/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String getHello() {
-        return "Hello World";
+        return "Welcome to the DevOps of 2025";
     }
 
 }
